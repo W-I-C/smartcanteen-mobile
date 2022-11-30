bbc1925210d9e77848732fa1eba2896e86249aca
castro <joaocastro2002@gmail.com>
2022-11-30 10:22:59 +0000

    merge

---
+++ app/src/main/java/pt/ipca/smartcanteen/MyOrders.kt
@@ -0,0 +1,28 @@
+package pt.ipca.smartcanteen
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.DefaultItemAnimator
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+class MyOrders : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.my_orders)
+
+        /** Orders **/
+        val myOrdersRecyclerView = findViewById<RecyclerView>(R.id.myorders_recycler_view)
+        val myOrders = mutableListOf<Order>(
+            Order("Francesinha", 5.0, 15.0, "Pronto"),
+            Order("Panado", 3.5, 5.0, "Em preparação"),
+            Order("Salada", 1.5, 1.0, "Em atraso"),
+        )
+
+        var myOrdersAdapter = OrdersAdapterRec(myOrders)
+        val linearLayoutManager = LinearLayoutManager(applicationContext)
+        myOrdersRecyclerView.layoutManager = linearLayoutManager
+        myOrdersRecyclerView.itemAnimator = DefaultItemAnimator()
+        myOrdersRecyclerView.adapter = myOrdersAdapter
+    }
+}
