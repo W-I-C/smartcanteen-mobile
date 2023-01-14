15f925c3fd19589533b4d232badea39f2b89b113
roberto <robertofmbarreto@gmail.com>
2023-01-14 12:02:59 +0000

    ementa done

---
+++ app/src/main/java/pt/ipca/smartcanteen/models/adapters/BarMenuMealsAdapterRec.kt
@@ -0,0 +1,4 @@
+package pt.ipca.smartcanteen.models.adapters
+
+class BarMenuMealsAdapterRec {
+}
---
+++ app/src/main/java/pt/ipca/smartcanteen/views/activities/ConsumerBarMenuActivity.kt
@@ -0,0 +1,11 @@
+package pt.ipca.smartcanteen.views.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ConsumerMenuActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_consumer_menu)
+    }
+}
