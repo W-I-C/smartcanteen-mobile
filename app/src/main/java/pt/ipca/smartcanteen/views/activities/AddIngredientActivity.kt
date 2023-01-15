8345337dfb72805fb8cc68f172d8c74f60362875
roberto <robertofmbarreto@gmail.com>
2023-01-15 00:56:37 +0000

    done employee menu

--- app/src/main/java/pt/ipca/smartcanteen/views/activities/AddIngredientActivity.kt
+++ app/src/main/java/pt/ipca/smartcanteen/views/activities/AddIngredientActivity.kt
@@ -1,10 +1,14 @@
 package pt.ipca.smartcanteen.views.activities
 
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import pt.ipca.smartcanteen.R
 
 class AddIngredientActivity: AppCompatActivity() {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
--- app/src/main/java/pt/ipca/smartcanteen/views/fragments/employee_fragments/EmployeeFragmentActivity.kt
+++ app/src/main/java/pt/ipca/smartcanteen/views/fragments/employee_fragments/EmployeeFragmentActivity.kt
@@ -5,7 +5,6 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.FragmentTransaction
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import pt.ipca.smartcanteen.R
-import pt.ipca.smartcanteen.views.fragments.consumer_fragments.MenuConsumerFragment
 import pt.ipca.smartcanteen.views.fragments.consumer_fragments.ProfileFragment
 
 class EmployeeFragmentActivity : AppCompatActivity() {
@@ -16,17 +15,19 @@ class EmployeeFragmentActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.bottom_navigation_employee)
 
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation_employee)
+
         val ft: FragmentTransaction = supportFragmentManager.beginTransaction()
-        ft.add(R.id.employee_fragment_container, EmployeeBarMenuFragment())
+        ft.add(R.id.employee_fragment_container, EmployeeMenuFragment(supportFragmentManager,bottomNavigationView))
         ft.commit()
 
-        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation_employee)
+
         bottomNavigationView.selectedItemId = R.id.menu_employee_main
         bottomNavigationView.setOnNavigationItemSelectedListener { item ->
             when (item.itemId) {
                 R.id.menu_employee_main -> {
                     val ft: FragmentTransaction = supportFragmentManager.beginTransaction()
-                    ft.replace(R.id.employee_fragment_container, EmployeeBarMenuFragment())
+                    ft.replace(R.id.employee_fragment_container, EmployeeMenuFragment(supportFragmentManager,bottomNavigationView))
                     ft.commit()
                 }
                 R.id.menu_employee_meals -> {
