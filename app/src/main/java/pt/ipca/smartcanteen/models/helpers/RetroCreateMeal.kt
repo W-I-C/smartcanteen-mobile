4c1dedf4205d5bcbeeb9c7e7ed5c808a3437062e
henrique <henriquecartucho@gmail.com>
2023-01-16 17:01:40 +0000

    ..

--- app/src/main/java/pt/ipca/smartcanteen/models/helpers/RetroCreateMeal.kt
+++ app/src/main/java/pt/ipca/smartcanteen/models/helpers/RetroCreateMeal.kt
@@ -1,4 +1,24 @@
 package pt.ipca.smartcanteen.models.helpers
 
-class RetroCreateMeal {
+import com.google.gson.annotations.SerializedName
+
+class RetroCreateMeal(
+@SerializedName("name") var name:String,
+@SerializedName("preparationtime") var time:String,
+@SerializedName("description") var description:String,
+@SerializedName("price") var price:Double,
+@SerializedName("allowedchanges") var allowedChanges: List<AllowedChange>
+)
+
+class AllowedChange(
+    @SerializedName("ingname") var ingName: String,
+    @SerializedName("ingdosage") var ingDosage: String,
+    @SerializedName("isremoveonly") var isRemoveOnly: Boolean,
+    @SerializedName("canbeincremented") var canBeIncremented: Boolean,
+    @SerializedName("canbedecremented") var canBeDecremented: Boolean,
+    @SerializedName("incrementlimit") var incrementLimit: Int,
+    @SerializedName("decrementlimit") var decrementLimit: Int
+)
+
+ {
 }
--- app/src/main/java/pt/ipca/smartcanteen/services/CreateMealService.kt
+++ app/src/main/java/pt/ipca/smartcanteen/services/CreateMealService.kt
@@ -1,4 +1,13 @@
 package pt.ipca.smartcanteen.services
 
-interface CreateMealService {
+import pt.ipca.smartcanteen.models.helpers.RetroCreateMeal
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Header
+
+interface CreateMealService{
+
+    @GET("api/v1/employee/create/meal/")
+    fun getCreateMeal(@Header("Authorization") authorization: String): Call<List<RetroCreateMeal>>
+
 }
